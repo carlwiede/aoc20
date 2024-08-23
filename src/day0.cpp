6d82b5721e9094a20073f2f147cf65948f13dc9a
Carl Wiede <guardsmancarl@gmail.com>
2024-08-23 10:41:14 +0200

    Polish template day

--- src/day0.cpp
+++ src/day0.cpp
@@ -1,6 +1,5 @@
 #include "day0.hpp"
 
-#include <fstream>
 #include <iostream>
 #include <string>
 
@@ -11,9 +10,22 @@ using namespace std;
 namespace day0 {
     void solve() {
         vector<string> input = Utils::readFile("input/day0.txt");
+        part1(input);
+        cout << endl;
+        part2(input);
+    }
 
+    void part1(vector<string> input) {
+        cout << "The input is:" << endl;
         for (string line : input) {
             cout << line << endl;
+        }
+    }
+
+    void part2(vector<string> input) {
+        cout << "The line length is:" << endl;
+        for (string line : input) {
+            cout << line.length() << endl;
         }
     }
 }
--- src/day0.hpp
+++ src/day0.hpp
@@ -1,8 +1,13 @@
 #ifndef DAY0_HPP
 #define DAY0_HPP
 
+#include <string>
+#include <vector>
+
 namespace day0 {
     void solve();
+    void part1(std::vector<std::string>);
+    void part2(std::vector<std::string>);
 }
 
 #endif
