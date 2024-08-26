7c5c3f114dfbd9e5003bb5b57a85a7f06133802d
Carl Wiede <guardsmancarl@gmail.com>
2024-08-26 20:34:16 +0200

    Solve day 2 part 1

--- src/day2.hpp
+++ src/day2.hpp
@@ -3,15 +3,30 @@
 
 #include "day.hpp"
 
+#include <algorithm>
+
 class Day2 : public Day {
     void part1(vector<string> input) override {
-        vector<string> parts = Utils::split(input.at(0), " ");
 
-        for (string part : parts) {
-            cout << part << endl;
+        int valid = 0;
+
+        for (string line : input) {
+            vector<string> parts = Utils::split(line, " ");
+            vector<string> numRange = Utils::split(parts.at(0), "-");
+            int min = stoi(numRange.at(0));
+            int max = stoi(numRange.at(1));
+
+            char special = parts.at(1).front();
+
+            string password = parts.at(2);
+            int occurrences = count(password.begin(), password.end(), special);
+
+            if (min <= occurrences && occurrences <= max) {
+                valid++;
+            }
         }
 
-        cout << "Part 1 not implemented." << endl;
+        cout << "There are " << valid << " valid passwords." << endl;
     }
 
     void part2(vector<string> input) override {
