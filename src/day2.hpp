b6fd385d054ccebff059d7d87997d5fe052dc64e
Carl Wiede <guardsmancarl@gmail.com>
2024-08-27 08:58:21 +0200

    Solve day 2 part 2

--- src/day2.hpp
+++ src/day2.hpp
@@ -30,7 +30,25 @@ class Day2 : public Day {
     }
 
     void part2(vector<string> input) override {
-        cout << "Part 2 not implemented." << endl;
+
+        int valid = 0;
+
+        for (string line : input) {
+            vector<string> parts = Utils::split(line, " ");
+            vector<string> numRange = Utils::split(parts.at(0), "-");
+            int first = stoi(numRange.at(0));
+            int second = stoi(numRange.at(1));
+
+            char special = parts.at(1).front();
+
+            string password = parts.at(2);
+
+            if ((password.at(first-1) == special) ^ (password.at(second-1) == special)) {
+                valid++;
+            }
+        }
+
+        cout << "There are " << valid << " valid passwords." << endl;
     }
 };
 
