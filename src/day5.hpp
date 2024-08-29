223110f9c1fe7a1643ab207369ebdd340e0881d1
Carl Wiede <guardsmancarl@gmail.com>
2024-08-29 15:53:39 +0200

    Solve day 5 part 2

--- src/day5.hpp
+++ src/day5.hpp
@@ -36,7 +36,39 @@ class Day5 : public Day {
     }
 
     void part2(vector<string> input) override {
-        cout << "Part 2 not implemented." << endl;
+
+        vector<int> allIds;
+
+        for (string pass : input) {
+            string row = pass.substr(0,7);
+            string col = pass.substr(7);
+
+            int rowValue = 0;
+            int colValue = 0;
+
+            // Calculate row
+            for (int i = 0; i < 7; i++) {
+                if (row.at(i) == 'B') rowValue += pow(2, 6-i);
+            }
+
+            // Calculate column
+            for (int i = 0; i < 3; i++) {
+                if (col.at(i) == 'R') colValue += pow(2, 2-i);
+            }
+
+            int idValue = rowValue * 8 + colValue;
+
+            allIds.push_back(idValue);
+        }
+
+        sort(allIds.begin(), allIds.end());
+
+        for (size_t i = 0; i < allIds.size() - 1; i++) {
+            if (allIds.at(i+1) != allIds.at(i) + 1) {
+                cout << "Your seat ID is " << allIds.at(i) + 1 << endl;
+                break;
+            }
+        }
     }
 };
 
